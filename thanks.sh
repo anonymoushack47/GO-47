789b7f840ce24fd11e8feacafaf9376842ad5ecc
anonymoushack47 <59957630+anonymoushack47@users.noreply.github.com>
2020-02-20 09:38:16 +0545

    Update thanks.sh

--- thanks.sh
+++ thanks.sh
@@ -7,12 +7,22 @@ r='\e[1;31m'
 y='\e[1;33m'
 ################
 echo ' 
- 
-                 _|    _|            _|  _|            
-                 _|    _|    _|_|    _|  _|    _|_|    
-                 _|_|_|_|  _|_|_|_|  _|  _|  _|    _|  
-                 _|    _|  _|        _|  _|  _|    _|  
-                 _|    _|    _|_|_|  _|  _|    _|_|' | lolcat
+HHHHHHHHH     HHHHHHHHH                   lllllll lllllll                  
+H:::::::H     H:::::::H                   l:::::l l:::::l                  
+H:::::::H     H:::::::H                   l:::::l l:::::l                  
+HH::::::H     H::::::HH                   l:::::l l:::::l                  
+  H:::::H     H:::::H      eeeeeeeeeeee    l::::l  l::::l    ooooooooooo   
+  H:::::H     H:::::H    ee::::::::::::ee  l::::l  l::::l  oo:::::::::::oo 
+  H::::::HHHHH::::::H   e::::::eeeee:::::eel::::l  l::::l o:::::::::::::::o
+  H:::::::::::::::::H  e::::::e     e:::::el::::l  l::::l o:::::ooooo:::::o
+  H:::::::::::::::::H  e:::::::eeeee::::::el::::l  l::::l o::::o     o::::o
+  H::::::HHHHH::::::H  e:::::::::::::::::e l::::l  l::::l o::::o     o::::o
+  H:::::H     H:::::H  e::::::eeeeeeeeeee  l::::l  l::::l o::::o     o::::o
+  H:::::H     H:::::H  e:::::::e           l::::l  l::::l o::::o     o::::o
+HH::::::H     H::::::HHe::::::::e         l::::::ll::::::lo:::::ooooo:::::o
+H:::::::H     H:::::::H e::::::::eeeeeeee l::::::ll::::::lo:::::::::::::::o
+H:::::::H     H:::::::H  ee:::::::::::::e l::::::ll::::::l oo:::::::::::oo 
+HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeee llllllllllllllll   ooooooooooo' | lolcat
 echo " "
 echo " "
 cat letter.txt
